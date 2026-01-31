abbcd49babe81354e1a1926f5bef1b96aa62c013
raedhashmi <raedhashmi15@gmail.com>
2026-01-31 12:56:04 +0500

    Fixed updater.

--- update version.py
+++ update version.py
@@ -2,12 +2,15 @@ with open('version.txt') as f:
     version = f.read().strip()
 
 major, patch = version.split('.')
-if int(patch) < 10:
-    new_version = f"{major}.0{int(patch) + 1}"
-else:
-    new_version = f"{major}.{int(patch) + 1}"
+major, patch = int(major), int(patch)
 
-# Write back
+if patch >= 32:
+    major += 1
+    patch = 0
+else:
+    patch += 1
+new_version = f"{major}.{patch:02d}"
+
 with open('version.txt', 'w') as f:
     f.write(new_version)
 print(f"\033[34m!\033[0m Updated version to: {new_version}")
